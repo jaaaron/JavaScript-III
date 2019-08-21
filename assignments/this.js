ae108c77fabd08a7f782a4b807125d36f0ac62cd
james <xjamesaaron@yahoo.com>
2019-08-21 12:54:12 -0700

    0.1

--- assignments/this.js
+++ assignments/this.js
@@ -21,11 +21,32 @@ fullName("James Aaron");
 // Principle 2
 // Implicit Binding
 // code example for Implicit Binding
-console.log("test");
+const nameGreeting = {
+    greeting: 'Hi',
+    sayHello: function(name) {
+      console.log(`${this.greeting} my name is ${name}`);
+      console.log(this);
+    }
+  };
+  nameGreeting.sayHello('James');
+
 // Principle 3
 // New Binding
 // code example for New Binding
-
+function wildAnimal(greeter) {
+    this.greeting = 'Hello ';
+    this.greeter = greeter;
+    this.speak = function() {
+      console.log(this.greeting + this.greeter);
+      console.log(this);
+    };
+  }
+  const tiger = new wildAnimal('Deer');
+  const deer = new wildAnimal('Whitetail');
+  tiger.speak()
+  deer.speak();
 // Principle 4
 // Explicit Binding
 // code example for Explicit Binding
+tiger.speak.call(deer); 
+deer.speak.apply(tiger);
